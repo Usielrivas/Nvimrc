a2b362b425d241e47486da03467d6c80760d6b30
Usiel Rivas <usielrivas00@gmail.com>
2021-10-17 07:00:33 -0600

    Se unifico la configuracion en un archivo init.lua

--- init.vim
+++ init.vim
@@ -1,9 +1,6 @@
 
 let &packpath=&runtimepath
 lua require('init')
-lua require('atajos')
-lua require('plugins')
-lua require('config-plugs')
 source ~/AppData/Local/nvim/.vimrc
 
 let $PATH = "C:\\Program Files\\Git\\usr\\bin;" .$PATH
--- lua/init.lua
+++ lua/init.lua
@@ -30,3 +30,84 @@ set.showmode= false
 -- vim.o.syntax = 'enable'
 
 --require('plugins')
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/AppData/Local/nvim/plug_vim')
+
+-- Los plugins van aquí
+-- ....
+-- "Cierra comillas y llaves automaticamnete
+Plug 'Raimondi/delimitMate'
+--" Tema ayu Personalizado
+Plug 'Usielrivas/ayu-vim'
+--" Coc Plug de auto completado
+Plug ( 'neoclide/coc.nvim', {branch= 'release'} )
+--"plug para Indentado y no perder las buenas practicas de estructurado
+Plug 'Yggdroot/indentLine'
+--"Optimiza vim para archivos grandes
+Plug 'vim-scripts/LargeFile'
+--"Plug para usar Git en vim
+Plug 'tpope/vim-fugitive'
+--"comentariovim
+Plug 'tpope/vim-commentary'
+--"surround
+Plug 'tpope/vim-surround'
+
+Plug ( 'junegunn/fzf', { [ 'do' ]= function()
+vim.fn['fzf#install']()
+end
+})
+
+Plug 'junegunn/fzf.vim'
+
+vim.call('plug#end')
+
+-- print('it is okay')
+
+-- "Atajos personalizados
+
+-- "Asigna leader a espacio
+vim.g.mapleader = ' '
+
+local map = vim.api.nvim_set_keymap
+
+-- "Guardar
+map('n', '<Leader>w', ':write<CR>', {noremap = true})
+-- cerrar
+map('n', 'q', ':qa<CR>', {noremap = true})
+--- "Cambio de pestaña/buffer
+map('n', '<Leader>z', ':bn!<CR>', {noremap = true})
+-- -- "Cierra buffer
+map('n', '<Leader>x', ':bd<CR>', {noremap = true})
+-- -- "Agranda y encoge pestaña
+map('n', '<Leader>-', '<c-w><', {noremap = true})
+map('n', '<Leader>+', '<c-w>>', {noremap = true})
+--" s{char}{char} to move to {char}{char}
+map('n', '<Leader>h', '<c-w>h', {noremap = true})
+map('n', '<Leader>l', '<c-w>l', {noremap = true})
+
+
+map('n', 'f', ':Files<CR>', {noremap = true})
+
+map('n', 'gs', ':GFiles?<CR>', {noremap = true})
+map('n', '<Leader>gc', ':Commits<CR>', {noremap = true})
+map('n', 'rg', ':Rg<CR>', {noremap = true})
+
+-- Configuracion de tema ayu
+
+vim.opt.termguicolors= true
+vim.cmd [[
+    colorscheme ayu
+]]
+-- "let ayucolor="dark"
+--
+
+
+-- "Ajustes para plug de Indentado
+vim.g[ 'indentLine_fileTypeExclude' ] = {'text','sh','help','terminal'} 
+vim.g[ 'gindentLine_bufNameExclude' ] = {'NERD_tree','term:.*'}
+vim.g[ 'gindentLine_defaultGroup' ] = 'SpecialKey'
+
+
+

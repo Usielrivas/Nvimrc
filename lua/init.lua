a63e78c40c09656c9c2af8067534401adbf83bf4
Usiel Rivas <usielrivas00@gmail.com>
2022-01-17 06:19:04 -0600

    added pretier

--- lua/init.lua
+++ lua/init.lua
@@ -94,10 +94,8 @@ vim.opt.termguicolors= true
 vim.cmd [[
     colorscheme dracula
     hi Normal guibg=#1d1f26
+    command! -nargs=0 Prettier :CocCommand prettier.formatFile
  ]]
--- "let ayucolor="dark"
---
-
 
 -- "Ajustes para plug de Indentado
 vim.g[ 'indentLine_fileTypeExclude' ] = {'text','sh','help','terminal'} 
@@ -105,7 +103,7 @@ vim.g[ 'gindentLine_bufNameExclude' ] = {'NERD_tree','term:.*'}
 vim.g[ 'gindentLine_defaultGroup' ] = 'SpecialKey'
 
 --"Automatiza la instalacion de plug para Coc
-vim.g[ 'coc_global_extensions' ]= { 'coc-css','coc-html','coc-emmet','coc-html-css-support','coc-tsserver','coc-vetur' }
+vim.g[ 'coc_global_extensions' ]= { 'coc-css','coc-html','coc-emmet','coc-html-css-support','coc-tsserver','coc-vetur','coc-phpls','coc-prettier' }
 
 
 --"Commit fzf

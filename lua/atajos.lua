d7a2e09b0c8c25be9ea09bb9536ceac9326075f7
Usiel Rivas <usielrivas00@gmail.com>
2022-04-26 06:43:47 -0600

    atajo para cambiar tamaño de ventana

--- lua/atajos.lua
+++ lua/atajos.lua
@@ -16,6 +16,8 @@ map("n", "<Leader>x", ":bd<CR>", {noremap = true})
 -- -- "Agranda y encoge pestaña
 map("n", "<Leader>-", "<c-w><", {noremap = true})
 map("n", "<Leader>+", "<c-w>>", {noremap = true})
+map("n", "<C-k>", "<c-w>+", {noremap = true})
+map("n", "<C-j>", "<c-w>-", {noremap = true})
 --" s{char}{char} to move to {char}{char}
 map("n", "<Leader>h", "<c-w>h", {noremap = true})
 map("n", "<Leader>j", "<c-w>j", {noremap = true})
